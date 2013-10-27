a55d0fd8908424634c0f1d31229df0814f100a03
Kushagra Agarwal <kushagra666@gmail.com>
2013-10-27 19:25:09 +0530

    experimenting

--- js/webview.js
+++ js/webview.js
@@ -1,10 +1,10 @@
 CocoonJS.App.onLoadInTheWebViewSucceed.addEventListener(function(pageURL) {
-    // Show the webview. By default, the webview is hidden.
-    CocoonJS.App.showTheWebView();
+  CocoonJS.App.forward("CocoonJS.App.show(0, 0, " + window.innerWidth/2 + "," + window.innerHeight + ");");
+  CocoonJS.App.showTheWebView();
 });
 
 CocoonJS.App.onLoadInTheWebViewFailed.addEventListener(function(pageURL) {
-    console.error("Could not load the HTML file in the webview");
+  console.error("Could not load the HTML file in the webview");
 });
 
 CocoonJS.App.loadInTheWebView("game_screen");
