a57a908e46fad0a2bb01bca58058c9ee8c715d0b
Kushagra Agarwal <kushagra666@gmail.com>
2013-10-27 20:10:52 +0530

    experimenting

--- js/ui-main.js
+++ js/ui-main.js
@@ -1,6 +1,6 @@
 ui.start_game.on('click', function() {
-  // CocoonJS.App.forward("mit.main.startGame();");
-  CocoonJS.App.forward("console.log('Start button touched');");
+  CocoonJS.App.forward("mit.main.startGame();");
+  // CocoonJS.App.forward("console.log('Start button touched');");
 
   return false;
 });
